564097d7a85f210e0dc3abf23f2768a05c31101e
Александр Шайковский <saden201@gmail.com>
2022-03-23 20:35:46 +0300

    Add dropdown history response

--- src/Components/Main/HistoryNavBar/HistoryItem/index.tsx
+++ src/Components/Main/HistoryNavBar/HistoryItem/index.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+import classNames from "classnames";
+import { FC, useCallback, useRef, useState } from "react";
+import { useOutside } from "../../../../Helpers";
 import { Icons } from "../../../MiniComponents/Icons";
 
 import "./index.css";
@@ -9,14 +11,72 @@ interface HistoryItemProps {
 }
 
 export const HistoryItem: FC<HistoryItemProps> = ({ status, name }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClickOpen = () => {
+    setIsOpen(true);
+  };
+
   return (
-    <div className="history-nav-bar__item">
-      {status ? (
-        <Icons icon="EllipseSuccess" alt="success" />
-      ) : (
-        <Icons icon="EllipseError" alt="error" />
-      )}
-      {name}
+    <div>
+      <div
+        className="history-nav-bar__item"
+        onClick={!isOpen ? onClickOpen : undefined}
+      >
+        <span>
+          {status ? (
+            <Icons
+              icon="EllipseSuccess"
+              alt="success"
+              className="history-nav-bar__status"
+            />
+          ) : (
+            <Icons
+              icon="EllipseError"
+              alt="error"
+              className="history-nav-bar__status"
+            />
+          )}
+          {name}
+        </span>
+        <Icons icon={"DragElement"} className="history-nav-bar__menu-icon" />
+      </div>
+      <div
+        className={classNames("history-nav-bar__item_drop", {
+          "history-nav-bar__item_active": isOpen,
+        })}
+      >
+        {isOpen && <DropDownContant isOpen={isOpen} setIsOpen={setIsOpen} />}
+      </div>
+    </div>
+  );
+};
+
+interface DropDownContantProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const DropDownContant: FC<DropDownContantProps> = ({ isOpen, setIsOpen }) => {
+  const closeDropDown = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
+
+  const dropDownContantRef = useRef<HTMLDivElement>(null);
+  useOutside(dropDownContantRef, closeDropDown);
+
+  return (
+    <div>
+      <div className="history-nav-bar__item_drop-elem_mt" />
+      <div ref={dropDownContantRef}>
+        <div></div>
+        <div className="history-nav-bar__item_drop-elem">Выполнить</div>
+        <div className="history-nav-bar__item_drop-elem">Скопировать</div>
+        <div className="history-nav-bar__item_drop-elem_decor" />
+        <div className="history-nav-bar__item_drop-elem history-nav-bar__item_drop-elem_danger">
+          Удалить
+        </div>
+      </div>
     </div>
   );
 };
--- src/Helpers/index.ts
+++ src/Helpers/index.ts
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 export function getCookie(name: string) {
   let matches = document.cookie.match(
     new RegExp(
@@ -7,4 +9,24 @@ export function getCookie(name: string) {
     )
   );
   return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+export function useOutside(
+  ref: React.RefObject<HTMLDivElement>,
+  onClickOutside: () => void
+) {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        onClickOutside();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ref]);
 }
